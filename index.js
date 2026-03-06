e26cbabf302c3ccd8c26ab88923518bfd0bcbc0f
John Katana <johnkatana624@gmail.com>
2026-03-06 17:41:27 +0300

    extra commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-}
+let body = document.body;
+let iconCart = document.querySelector(".icon-cart");
+let closeCart = document.querySelector(".close");
+iconCart.addEventListener("click", () => {
+  body.classList.toggle("showCart");
+});
+closeCart.addEventListener("click", () => {
+  body.classList.toggle("showCart");
+});
