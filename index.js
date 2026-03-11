e5df063b4697984bc605c16e287318edf7a7c225
John Katana <johnkatana624@gmail.com>
2026-03-11 14:13:42 +0300

    toady

--- index.js
+++ index.js
@@ -16,29 +16,6 @@ closeCart.addEventListener("click", () => {
   body.classList.toggle("showCart");
 });
 
-const addDataToCart = () => {
-  productListHTML.innerHTML = "";
-  productList.forEach((product) => {
-    const productItem = document.createElement("div");
-    productItem.classList.add("product-item");
-    productItem.innerHTML = `
-      <img src="${product.image}" alt="${product.name}">
-      <h3>${product.name}</h3>
-      <p>Ksh ${product.price}</p>
-      <button class="button-cart" data-id="${product.id}">ADD TO CART</button>
-    `;
-    productListHTML.appendChild(productItem);
-  });
-};
-productListHTML.addEventListener("click", (event) => {
-  const target = event.target;
-  if (target.classList.contains("button-cart")) {
-    const id = target.dataset.id;
-    const prod = productList.find((p) => p.id == id);
-    if (prod) addToCart(prod);
-  }
-});
-
 const renderProducts = () => {
   fetch("products.json")
     .then((response) => response.json())
