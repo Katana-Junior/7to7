70d64ce231056a8a8fc6fbcc7ce3e9b2f7d7ff20
John Katana <johnkatana624@gmail.com>
2026-03-12 14:35:24 +0300

    toadys commit

--- index.js
+++ index.js
@@ -22,9 +22,18 @@ const renderProducts = () => {
     .then((data) => {
       productList = data.products;
       console.log(productList);
-      addDataToCart(); // populate the DOM after loading
     })
     .catch((error) => console.error("Error fetching products:", error));
 };
 
 renderProducts();
+
+const aboutBtn = document.querySelector(".about-btn");
+aboutBtn.addEventListener("click", () => {
+  document.getElementById("content").innerHTML = `
+  <h2>About Us</h2>
+  <p>Welcome to our online store! We are passionate about providing high-quality products that enhance your lifestyle.
+   Our mission is to offer a wide range of items that cater to your needs and preferences,
+    all while ensuring excellent customer service. Thank you for choosing us for your shopping experience!</p>
+`;
+});
